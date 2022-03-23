efe4b57b9daab8cb02a2ad62fb6116f1c92439f7
senait <senait.ekubai@capgemini.com>
2022-03-23 16:09:27 +0000

    initial commit

---
+++ src/test/java/com/academy/springBootSweetTreatsApp/SpringBootSweetTreatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.springBootSweetTreatsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSweetTreatsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
