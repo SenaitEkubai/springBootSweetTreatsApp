d6a16b6e426e3503e24ba59933c32d52ec8ad1d8
senait <senait.ekubai@capgemini.com>
2022-03-23 17:34:00 +0000

    order class

---
+++ src/main/java/com/academy/springBootSweetTreatsApp/models/Order.java
@@ -0,0 +1,29 @@
+package com.academy.springBootSweetTreatsApp.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Data
+@Setter
+@Getter
+public class Order {
+    private UUID id;
+    private LocalTime deliveryTime;
+    private int distanceInMiles;
+    private boolean requireRefrigeration;
+
+    public Order(String deliveryTime, int distanceInMiles, boolean requireRefrigeration) {
+        this.id = UUID.randomUUID();
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+        LocalTime time = LocalTime.parse(deliveryTime, formatter);
+        this.deliveryTime = time;
+        this.requireRefrigeration = requireRefrigeration;
+        this.distanceInMiles = distanceInMiles;
+    }
+
+}
