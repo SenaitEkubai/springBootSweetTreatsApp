0c92a5aa7ddf83f65bb0230a128bda915bed7cba
senait <senait.ekubai@capgemini.com>
2022-03-23 17:34:22 +0000

    order service implementation

---
+++ src/main/java/com/academy/springBootSweetTreatsApp/services/OrderServiceImpl.java
@@ -0,0 +1,28 @@
+package com.academy.springBootSweetTreatsApp.services;
+
+import com.academy.springBootSweetTreatsApp.models.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class OrderServiceImpl implements OrderService{
+    @Override
+    public void createOrder() {
+
+    }
+
+    @Override
+    public void editOrder() {
+
+    }
+
+    @Override
+    public void deleteOrder() {
+
+    }
+
+    @Override
+    public List<Order> getOrders() {
+        return null;
+    }
+}
