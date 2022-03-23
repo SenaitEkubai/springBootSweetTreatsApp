dadeb6a0ccc63699c7e2260b46a5619ff6f45248
senait <senait.ekubai@capgemini.com>
2022-03-23 17:35:53 +0000

    formatted application class

--- src/main/java/com/academy/springBootSweetTreatsApp/SpringBootSweetTreatsAppApplication.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/SpringBootSweetTreatsAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSweetTreatsAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSweetTreatsAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSweetTreatsAppApplication.class, args);
+    }
 
 }
