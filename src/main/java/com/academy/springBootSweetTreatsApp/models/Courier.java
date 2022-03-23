a11ac6d2cca04c024b152820cde6cf9d4d206de8
senait <senait.ekubai@capgemini.com>
2022-03-23 17:32:40 +0000

    courier class

---
+++ src/main/java/com/academy/springBootSweetTreatsApp/models/Courier.java
@@ -0,0 +1,48 @@
+package com.academy.springBootSweetTreatsApp.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.UUID;
+
+@Data
+@Setter
+@Getter
+public class Courier {
+    private UUID id;
+    private String name;
+    private LocalTime shiftStart;
+    private LocalTime shiftEnd;
+    private int distanceInMiles;
+    private boolean refrigerated;
+    private double chargePerMile;
+    private boolean preferred;
+
+
+    public Courier( String name, String shiftStart, String shiftEnd, int distanceInMiles, boolean refrigerated, double chargePerMile, boolean preferred) {
+        //Random rand = new Random();
+        this.id= UUID.randomUUID();
+        this.name = name;
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
+        LocalTime start = LocalTime.parse(shiftStart, formatter);
+        LocalTime end = LocalTime.parse(shiftEnd, formatter);
+        this.shiftStart = start;
+        this.shiftEnd = end;
+        this.distanceInMiles = distanceInMiles;
+        this.refrigerated = refrigerated;
+        this.chargePerMile = chargePerMile;
+        this.preferred = preferred;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + " charges  " + this.getChargePerMile();
+    }
+
+}
+
+
