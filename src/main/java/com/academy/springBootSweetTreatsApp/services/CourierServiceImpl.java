bde8ed9de39920dda487d5d1c9c86279a27fcd14
senait <senait.ekubai@capgemini.com>
2022-03-24 15:52:13 +0000

    added getOneCourier method

--- src/main/java/com/academy/springBootSweetTreatsApp/services/CourierServiceImpl.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/services/CourierServiceImpl.java
@@ -3,17 +3,18 @@ package com.academy.springBootSweetTreatsApp.services;
 import com.academy.springBootSweetTreatsApp.models.Courier;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
-public class CourierServiceImpl implements CourierService{
+public class CourierServiceImpl implements CourierService {
+    private static final Logger LOGGER = Logger.getLogger(CourierServiceImpl.class.getName());
     List<Courier> courierList = new ArrayList<>();
 
     @Override
     public void createACourier(Courier courier) {
-     courierList.add(courier);
+        courierList.add(courier);
     }
 
     @Override
@@ -35,4 +36,23 @@ public class CourierServiceImpl implements CourierService{
     public List<Courier> getCouriers() {
         return courierList;
     }
+
+    @Override
+    public Courier getOneCourier(UUID id) {
+        Courier courier = null;
+
+            for (int i = 0; i < courierList.size(); i++) {
+                if(courierList.get(i).getId().equals(id)){
+                    courier= courierList.get(i);
+                }
+
+            }
+
+        return courier;
+    }
+
+
+    // suitable couriers
+
+
 }
