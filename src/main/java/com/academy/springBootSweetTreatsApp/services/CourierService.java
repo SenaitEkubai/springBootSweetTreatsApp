d3c15a5bf3f4dcfeda2acad76d91f5b3e2976c47
senait <senait.ekubai@capgemini.com>
2022-03-24 15:51:23 +0000

    added new abstract methods

--- src/main/java/com/academy/springBootSweetTreatsApp/services/CourierService.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/services/CourierService.java
@@ -1,16 +1,24 @@
 package com.academy.springBootSweetTreatsApp.services;
 
 import com.academy.springBootSweetTreatsApp.models.Courier;
+import com.academy.springBootSweetTreatsApp.models.Order;
+import org.apache.el.stream.Stream;
 
 import java.util.List;
+import java.util.UUID;
+
 
 public interface CourierService {
     void createACourier(Courier courier);
+
     void createManyCouriers(List<Courier> couriers);
+
+    List<Courier> getCouriers();
+
+    Courier getOneCourier(UUID id);
 
     void editCourier();
 
     void deleteCourier();
 
-    List<Courier> getCouriers();
 }
