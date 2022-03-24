b82a21f594319de15896611199497e3682d0c08b
senait <senait.ekubai@capgemini.com>
2022-03-24 15:52:54 +0000

    added get one order method

--- src/main/java/com/academy/springBootSweetTreatsApp/services/OrderService.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/services/OrderService.java
@@ -1,15 +1,21 @@
 package com.academy.springBootSweetTreatsApp.services;
 
+import com.academy.springBootSweetTreatsApp.models.Courier;
 import com.academy.springBootSweetTreatsApp.models.Order;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
-    void createOrder();
+
+
+    void createOrder(Order order);
 
     void editOrder();
 
     void deleteOrder();
 
-    List<Order> getOrders();
+   List<Order> getOrders();
+
+   Order getOneOrder(UUID id);
 }
