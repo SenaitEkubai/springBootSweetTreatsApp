8e66916475459e88d747e3ad4aca2f572c3617e7
senait <senait.ekubai@capgemini.com>
2022-04-05 16:22:38 +0100

    removed constructor

--- src/main/java/com/academy/springBootSweetTreatsApp/models/Order.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/models/Order.java
@@ -3,25 +3,19 @@ package com.academy.springBootSweetTreatsApp.models;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Data
+@Document
 public class Order {
-    private UUID id;
+    @Id
+    private String id;
     private LocalTime deliveryTime;
     private int distanceInMiles;
     private boolean requireRefrigeration;
-
-    public Order(String deliveryTime, int distanceInMiles, boolean requireRefrigeration) {
-        this.id = UUID.randomUUID();
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
-        LocalTime time = LocalTime.parse(deliveryTime, formatter);
-        this.deliveryTime = time;
-        this.requireRefrigeration = requireRefrigeration;
-        this.distanceInMiles = distanceInMiles;
-    }
-
 }
