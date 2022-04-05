4708ebe6f9be8cb290e2d235764edf45a4c14f55
senait <senait.ekubai@capgemini.com>
2022-04-05 16:28:56 +0100

    changed method arguments from id to order

--- src/main/java/com/academy/springBootSweetTreatsApp/services/SweetTreatsService.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/services/SweetTreatsService.java
@@ -1,12 +1,13 @@
 package com.academy.springBootSweetTreatsApp.services;
 
 import com.academy.springBootSweetTreatsApp.models.Courier;
+import com.academy.springBootSweetTreatsApp.models.Order;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface SweetTreatsService {
-    List<Courier> suitableCouriers(UUID id);
+    List<Courier> suitableCouriers(Order order);
 
-    Courier cheapestCourier(UUID id);
+    Courier cheapestCourier(Order order);
 }
