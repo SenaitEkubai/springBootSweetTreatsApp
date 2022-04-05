972097dffbfd6aa38bbbdc083de6682c7e7722ac
senait <senait.ekubai@capgemini.com>
2022-04-05 16:26:08 +0100

    created new methods

--- src/main/java/com/academy/springBootSweetTreatsApp/controllers/CourierController.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/controllers/CourierController.java
@@ -1,63 +1,66 @@
 package com.academy.springBootSweetTreatsApp.controllers;
 
 import com.academy.springBootSweetTreatsApp.models.Courier;
-import com.academy.springBootSweetTreatsApp.services.CourierService;
+import com.academy.springBootSweetTreatsApp.models.Order;
 import com.academy.springBootSweetTreatsApp.services.CourierServiceImpl;
+import com.academy.springBootSweetTreatsApp.services.SweetTreatsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("sweettreats")
 public class CourierController {
+
     @Autowired
     CourierServiceImpl courierService;
 
-
-    // all couriers
+    @Autowired
+    SweetTreatsServiceImpl sweetTreatsService;
 
     @RequestMapping(path = "/couriers")
     List<Courier> getAllCouriers() {
         return courierService.getCouriers();
     }
 
-
-   /* //  create one courier
-
     @PostMapping(path = "couriers/addcourier")
     ResponseEntity<String> addOneCourier(@RequestBody Courier courier) {
         courierService.createACourier(courier);
         return new ResponseEntity<>("Courier is added successfully", HttpStatus.OK);
-    }*/
-    //  create one courier
-
-    @PostMapping(path = "couriers/addcourier")
-    ResponseEntity<String> addOneCourier(@RequestBody Courier courier) {
-        courierService.addCourier(courier);
-        return new ResponseEntity<>("Courier is added successfully", HttpStatus.OK);
     }
 
-    // create many couriers at once
-
     @PostMapping(path = "couriers/addcouriers")
-    ResponseEntity<String> addManyCouriers(@RequestBody List<Courier> couriers) {
+    ResponseEntity<?> addManyCouriers(@RequestBody List<Courier> couriers) {
         courierService.createManyCouriers(couriers);
         return new ResponseEntity<>("Couriers added successfully", HttpStatus.OK);
     }
 
-
-    // get one courier by id
-
     @RequestMapping(path = "couriers/{id}")
-    Courier getOneCourier(@PathVariable("id") UUID id) {
-        return courierService.getOneCourier(id);
+    Optional<Courier> getOneCourierById(@PathVariable("id") String id) {
+        return courierService.getCourierById(id);
     }
 
+    @DeleteMapping(path = "couriers/{id}")
+    void deleteOneCourierById(@PathVariable("id") String id) {
+        courierService.deleteCourier(id);
+    }
+
+
+    @RequestMapping(path = "/suitablecouriers")
+    List<Courier> getSuitableCouriers(@RequestBody Order order) {
+        return sweetTreatsService.suitableCouriers(order);
+    }
+
+    @RequestMapping(path = "/cheapestcourier")
+    Courier getCheapestCourier(@RequestBody Order order) {
+        return sweetTreatsService.cheapestCourier(order);
+
+    }
 }
