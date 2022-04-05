a2b80ec2bba74f6cea64a906166ae51de8e6c022
senait <senait.ekubai@capgemini.com>
2022-04-05 16:22:31 +0100

    removed constructor

--- src/main/java/com/academy/springBootSweetTreatsApp/models/Courier.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/models/Courier.java
@@ -2,6 +2,8 @@ package com.academy.springBootSweetTreatsApp.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
 import java.time.LocalTime;
@@ -9,8 +11,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Data
+@Document
 public class Courier {
-    private UUID id;
+    @Id
+    private String id;
     private String name;
     private LocalTime shiftStart;
     private LocalTime shiftEnd;
@@ -18,24 +22,6 @@ public class Courier {
     private boolean refrigerated;
     private double chargePerMile;
     private boolean preferred;
-
-
-    public Courier(@JsonProperty("id") UUID id,@JsonProperty("name") String name, @JsonProperty("shiftStart") String shiftStart, @JsonProperty("shiftEnd") String shiftEnd, @JsonProperty("distanceInMiles") int distanceInMiles,
-                               @JsonProperty("refrigerated") boolean refrigerated, @JsonProperty("chargePerMile") double chargePerMile, @JsonProperty("preferred") boolean preferred) {
-        id = UUID.randomUUID();
-         this.id=id;
-        this.name = name;
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_TIME;
-        LocalTime start = LocalTime.parse(shiftStart, formatter);
-        LocalTime end = LocalTime.parse(shiftEnd, formatter);
-        this.shiftStart = start;
-        this.shiftEnd = end;
-        this.distanceInMiles = distanceInMiles;
-        this.refrigerated = refrigerated;
-        this.chargePerMile = chargePerMile;
-        this.preferred = preferred;
-    }
-
     @Override
     public String toString() {
         return this.getName() + " charges  " + this.getChargePerMile();
