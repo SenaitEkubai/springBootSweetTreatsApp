192939ef5f00e866cc0c060631f2644a8f43a2b4
senait <senait.ekubai@capgemini.com>
2022-04-05 16:30:07 +0100

    cleaned and refactored code

--- src/main/java/com/academy/springBootSweetTreatsApp/controllers/OrderController.java
+++ src/main/java/com/academy/springBootSweetTreatsApp/controllers/OrderController.java
@@ -1,40 +1,42 @@
 package com.academy.springBootSweetTreatsApp.controllers;
+
 import com.academy.springBootSweetTreatsApp.models.Order;
-import com.academy.springBootSweetTreatsApp.services.OrderService;
+import com.academy.springBootSweetTreatsApp.services.OrderServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("sweettreats/orders")
 public class OrderController {
-
-    OrderService orderService;
-
-    // get order
+    @Autowired
+    OrderServiceImpl orderService;
 
     @RequestMapping(path = "/")
-    List<Order> getOrders() {
+    List<Order> getAllOrders() {
         return orderService.getOrders();
     }
 
-    // get one order by id
-
     @RequestMapping(path = "/{id}")
-    Order getOneOrder(@PathVariable("id") UUID id) {
+    Optional<Order> getOneOrderById(@PathVariable("id") String id) {
         return orderService.getOneOrder(id);
     }
 
-    // add order
-
     @PostMapping(path = "/addorder")
-    ResponseEntity<String> createOrder(@RequestBody Order order) {
+    ResponseEntity<String> createOneOrder(@RequestBody Order order) {
         orderService.createOrder(order);
         return new ResponseEntity<>("order created successfully", HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/deleteOrder/{id}")
+    ResponseEntity<String> deleteOneOrderById(@PathVariable("id") String id){
+        orderService.deleteOrder(id);
+        return new ResponseEntity<>("order deleted successfully", HttpStatus.OK);
     }
 }
